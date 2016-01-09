5d96ce31333f9e1aa1c2b7c0c1aad998a7829e86
xfjx <xfjx@m4tr1xx.de>
2016-01-09 17:46:58 +0100

    First version.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # homebridge-wunderground
-Homematic plugin for Weather Underground: https://github.com/nfarina/homebridge
+Weather Underground plugin for homebridged: https://github.com/nfarina/homebridge
+
+This is a very basic plugin for Nfarina's wonderfull [Homebridge project](https://github.com/nfarina/homebridge). It will fetch current weather conditions from [Weather Underground](http://wunderground.com) and provide temperature and humidity information for HomeKit.
+
+You can easily use these values as conditions for triggers or just look at them via HomeKit enabled Apps on your iOS device or even ask Siri for them.
+
+# Installation
+
+1. Install homebridge using: npm install -g homebridge
+2. Install this plugin using: npm install -g homebridge-wunderground
+3. Update your configuration file. See the sample below.
+
+All you need now is a developer key for Weather Underground which can be easily created [here](http://www.wunderground.com/weather/api/).
+
+# Configuration
+
+Configuration sample:
+
+ ```
+Add the following information to your config file. Make sure to add your API key and provice your city or postal code.
+
+"accessories": [
+    {
+      "accessory": "WUNDERGROUND",
+      "name": "Weather Underground",
+      "device": "WURecklinghausen",
+      "key": "xxxxxxxxxxxx",
+      "city": "Germany/Recklinghausen"
+      }
+    ]
+```
---
+++ index.js
@@ -0,0 +1,76 @@
+"use strict";
+var Wunderground = require('wundergroundnode');
+var Service, Characteristic;
+
+var temperatureService;
+var humidityService;
+
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("homebridge-wunderground", "WUNDERGROUND", WUTemphum);
+}
+
+function WUTemphum(log, config) {
+    this.log = log;
+    this.wunderground = new Wunderground(config['key']);
+    this.name = config['name'];
+    this.city = config['city'];
+    this.timestampOfLastUpdate = 0;
+}
+
+WUTemphum.prototype = {
+
+    getStateHumidity: function(callback){    
+	callback(null, this.humidity);
+    },
+
+    getState: function (callback) {
+	// Only fetch new data once per minute
+	var that = this;
+	if (this.timestampOfLastUpdate + 60 < (Date.now() / 1000 | 0))
+	{
+	    that.wunderground.conditions().request(that.city, function(err, response){
+		that.timestampOfLastUpdate = Date.now() / 1000 | 0;
+		that.log('Successfully fetched weather data from wunderground.com');
+    		that.temperature = response['current_observation']['temp_c'];
+   		that.humidity = parseInt(response['current_observation']['relative_humidity'].substr(0, response['current_observation']['relative_humidity'].length-1));
+	    });
+	}
+	temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
+    	humidityService.setCharacteristic(Characteristic.CurrentRelativeHumidity, this.humidity);
+	callback(null, this.temperature);
+    },
+
+    identify: function (callback) {
+        this.log("Identify requested!");
+        callback(); // success
+    },
+
+    getServices: function () {
+        var informationService = new Service.AccessoryInformation();
+
+        informationService
+                .setCharacteristic(Characteristic.Manufacturer, "HomeBridge")
+                .setCharacteristic(Characteristic.Model, "Weather Underground")
+                .setCharacteristic(Characteristic.SerialNumber, this.city);
+
+        temperatureService = new Service.TemperatureSensor(this.name);
+        temperatureService
+                .getCharacteristic(Characteristic.CurrentTemperature)
+                .on('get', this.getState.bind(this));
+
+        humidityService = new Service.HumiditySensor(this.name);
+        humidityService
+                .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+                .on('get', this.getStateHumidity.bind(this));
+
+        return [informationService, temperatureService, humidityService];
+
+	return this.services;
+    }
+};
+
+if (!Date.now) {
+    Date.now = function() { return new Date().getTime(); }
+}
