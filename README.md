deba8beb68799bb8987a4669ada9f197e38ef5ae
Kevin Harwood <kcharwood@gmail.com>
2016-06-02 12:27:47 -0500

    Refactored to be an updating weather station

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
-# homebridge-wunderground
-Weather Underground plugin for homebridge: https://github.com/nfarina/homebridge
+# homebridge-weather-station
+Weather Underground Weather Station plugin for homebridge: https://github.com/nfarina/homebridge
 
-This is a very basic plugin for Nfarina's wonderfull [Homebridge project](https://github.com/nfarina/homebridge). It will fetch current weather conditions from [Weather Underground](http://wunderground.com) and provide temperature and humidity information for HomeKit.
+This is a weather station plugin for Nfarina's [Homebridge project](https://github.com/nfarina/homebridge). It will fetch current weather conditions from [Weather Underground](http://wunderground.com) and provide temperature, humidity, and clear/rain/snow condition information to HomeKit. These values update every 4 minutes with the latest conditions from wunderground.com
 
 You can use these values as conditions for triggers or just look at them via HomeKit enabled Apps on your iOS device or even ask Siri for them.
 
-It will get new data only once per minute.
+## Weather Conditions
+
+Currently, the current weather conditions are exposed in two properties, `WeatherCondition` and `WeatherConditionValue`. `WeatherCondition` is a read-only string representation of the current weather conditions, and can be one of the Forecast Description Phrases listed on this [page](https://www.wunderground.com/weather/api/d/docs?d=resources/phrase-glossary).
+
+`WeatherConditionValue` is an enum value that currently represents one of three values:
+
+| Condition              | Enum Value			                                                      |
+| -------------------    | -------- |
+| Other   		         | 0 |
+| Rain  			     | 1 |
+| Snow			    	 | 2 |
+
+You can combine this with the [homebridge-suncalc](https://github.com/kcharwood/homebridge-suncalc) to create rules that turn on and off the lights when its raining during the day.
 
 # Installation
 
 1. Install homebridge using: npm install -g homebridge
-2. Install this plugin using: npm install -g homebridge-wunderground
+2. Install this plugin using: npm install -g homebridge-weather-station
 3. Update your configuration file. See the sample below.
 
 All you need now is a developer key for Weather Underground which can be easily created [here](http://www.wunderground.com/weather/api/).
@@ -19,17 +31,17 @@ All you need now is a developer key for Weather Underground which can be easily 
 
 Configuration sample:
 
- ```
+
 Add the following information to your config file.
 Make sure to add your API key and provice your city or postal code.
 
+```json
 "accessories": [
-    {
-      "accessory": "WUNDERGROUND",
-      "name": "Weather Underground",
-      "device": "WURecklinghausen",
-      "key": "xxxxxxxxxxxx",
-      "city": "Germany/Recklinghausen"
-      }
+	    {
+	      "accessory": "WUWeatherStation",
+	      "name": "Weather Station",
+	      "key": "XXXXXXXXXXXXXXX",
+	      "city": "78613"
+	    }
     ]
 ```
--- index.js
+++ index.js
@@ -1,46 +1,78 @@
 "use strict";
 var Wunderground = require('wundergroundnode');
+var inherits = require('util').inherits;
 var Service, Characteristic;
 
-var temperatureService;
-var humidityService;
+var weatherStationService;
+
+var WeatherConditionValue
+var WeatherCondition
+var WeatherStation
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-wunderground", "WUNDERGROUND", WUTemphum);
+    homebridge.registerAccessory("homebridge-wunderground", "WUWeatherStation", WUWeatherStation);
+	
+    WeatherConditionValue = function() {
+      Characteristic.call(this, 'Weather Condition Value', 'cd65a9ab-85ad-494a-b2bd-2f380084134c');
+      this.setProps({
+        format: Characteristic.Formats.UINT8,
+        maxValue: 2,
+        minValue: 0,
+        minStep: 1,
+        perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+      });
+      this.value = this.getDefaultValue();
+    };
+    inherits(WeatherConditionValue, Characteristic);
+	
+    WeatherCondition = function() {
+      Characteristic.call(this, 'Weather Condition', 'cd65a9ab-85ad-494a-b2bd-2f380084134d');
+      this.setProps({
+        format: Characteristic.Formats.STRING,
+        perms: [Characteristic.Perms.READ]
+      });
+      this.value = this.getDefaultValue();
+    };
+    inherits(WeatherCondition, Characteristic);
+	
+    WeatherStation = function(displayName, subtype) {
+  	  Service.call(this, displayName, 'debf1b79-312e-47f7-bf82-993d9950f3a2', subtype);
+
+  	  // Required Characteristics
+  	  this.addCharacteristic(WeatherCondition);
+  	  this.addCharacteristic(WeatherConditionValue);
+	  this.addCharacteristic(Characteristic.CurrentRelativeHumidity);
+	  this.addCharacteristic(Characteristic.CurrentTemperature);
+
+  	  // Optional Characteristics
+  	  this.addOptionalCharacteristic(Characteristic.StatusActive);
+  	  this.addOptionalCharacteristic(Characteristic.Name);
+  	};
+  	inherits(WeatherStation, Service);
 }
 
-function WUTemphum(log, config) {
+function WUWeatherStation(log, config) {
     this.log = log;
     this.wunderground = new Wunderground(config['key']);
     this.name = config['name'];
     this.city = config['city'];
     this.timestampOfLastUpdate = 0;
+	
+    this.informationService = new Service.AccessoryInformation();
+
+    this.informationService
+            .setCharacteristic(Characteristic.Manufacturer, "HomeBridge")
+            .setCharacteristic(Characteristic.Model, "Weather Underground")
+            .setCharacteristic(Characteristic.SerialNumber, this.city);
+
+	this.weatherStationService = new WeatherStation(this.name)	
+	
+	this.updateWeatherConditions();
 }
 
-WUTemphum.prototype = {
-
-    getStateHumidity: function(callback){    
-	callback(null, this.humidity);
-    },
-
-    getState: function (callback) {
-	// Only fetch new data once per minute
-	var that = this;
-	if (this.timestampOfLastUpdate + 60 < (Date.now() / 1000 | 0))
-	{
-	    that.wunderground.conditions().request(that.city, function(err, response){
-		that.timestampOfLastUpdate = Date.now() / 1000 | 0;
-		that.log('Successfully fetched weather data from wunderground.com');
-    		that.temperature = response['current_observation']['temp_c'];
-   		that.humidity = parseInt(response['current_observation']['relative_humidity'].substr(0, response['current_observation']['relative_humidity'].length-1));
-	    });
-	}
-	temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
-    	humidityService.setCharacteristic(Characteristic.CurrentRelativeHumidity, this.humidity);
-	callback(null, this.temperature);
-    },
+WUWeatherStation.prototype = {
 
     identify: function (callback) {
         this.log("Identify requested!");
@@ -48,37 +80,46 @@ WUTemphum.prototype = {
     },
 
     getServices: function () {
-        var informationService = new Service.AccessoryInformation();
-
-        informationService
-                .setCharacteristic(Characteristic.Manufacturer, "HomeBridge")
-                .setCharacteristic(Characteristic.Model, "Weather Underground")
-                .setCharacteristic(Characteristic.SerialNumber, this.city);
-
-        temperatureService = new Service.TemperatureSensor(this.name);
-        temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .on('get', this.getState.bind(this));
-
-        temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .setProps({minValue: -50});
-        
-        temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .setProps({maxValue: 50});
-
-        humidityService = new Service.HumiditySensor(this.name);
-        humidityService
-                .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-                .on('get', this.getStateHumidity.bind(this));
-
-        return [informationService, temperatureService, humidityService];
-
-	return this.services;
-    }
+        return [this.informationService, this.weatherStationService];
+    },
+	
+	updateWeatherConditions: function() {
+		var that = this
+		
+	    that.wunderground.conditions().request(that.city, function(err, response){
+			that.timestampOfLastUpdate = Date.now() / 1000 | 0;
+    		that.temperature = response['current_observation']['temp_c'];
+			let conditionIcon = response['current_observation']['icon']
+			that.condition = response['current_observation']['weather']
+			switch (conditionIcon) {									
+			case "rain":
+			case "tstorm":
+			case "tstorms":
+				that.conditionValue = 1
+				break;
+			case "snow":
+			case "sleet":
+			case "flurries":
+				that.conditionValue = 2
+				break;
+			default:
+				that.conditionValue = 0
+				break;
+			}
+   			that.humidity = parseInt(response['current_observation']['relative_humidity'].substr(0, response['current_observation']['relative_humidity'].length-1));
+			
+			let humidityString = response['current_observation']['relative_humidity']
+			let temperatureString = response['current_observation']['temperature_string']
+			let uv = response['current_observation']['UV']
+			that.log("Current Weather Conditions: " + that.condition + ", " + temperatureString + ", " + humidityString + " humidity, UV: " + uv)
+			
+			that.weatherStationService.setCharacteristic(WeatherConditionValue,that.conditionValue);
+			that.weatherStationService.setCharacteristic(WeatherCondition,that.condition);
+			that.weatherStationService.setCharacteristic(Characteristic.CurrentTemperature, that.temperature);
+			that.weatherStationService.setCharacteristic(Characteristic.CurrentRelativeHumidity, that.humidity);
+	    });
+		
+		// wunderground limits to 500 api calls a day. Making a call every 4 minutes == 360 calls
+		setTimeout(this.updateWeatherConditions.bind(this), 4 * 60 * 1000);
+	}
 };
-
-if (!Date.now) {
-    Date.now = function() { return new Date().getTime(); }
-}
